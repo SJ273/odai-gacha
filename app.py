0a43679058e4204b3df7e9f5b6babc1237d0600f
SJ273 <251zs03534@student.zen.ac.jp>
2026-08-21 23:30:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
-DATABASE_URL=postgresql://postgres.uzptejkchlbrwivqlaiw:yCU5reS3PzyvnjxF@aws-0-ap-southeast-2.pooler.supabase.com:5432/postgres
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
